fcaaddb277185e6e5bd651f067cd5fe5b8a42078
eaylward8 <eaylward8@gmail.com>
2015-11-06 16:01:54 -0500

    Fixed countdown.rb

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  #while number > 0
+  while number > 0 do
+    #puts number
+    puts "#{number} SECOND(S)!"
+    #increment by -1
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number)
+  while number > 0 do
+    #puts number
+    puts "#{number} SECOND(S)!"
+    #increment by -1
+    number -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
 end
